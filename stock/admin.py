d7f107f61fee577d886b032348558a716791380a
Bruno Alves <Bruno6821@gmail.com>
2021-11-04 19:58:35 -0300

    Adicionando comentários aos Admins

--- stock/admin.py
+++ stock/admin.py
@@ -4,11 +4,21 @@ from .models import Stock, StockItems
 
 
 class StockItemsInline(admin.TabularInline):
+    """
+    Criaremos uma uma linha com os dados da Model StockItems,
+    o atributo extra = 0 é o número de linhas que podemos adicionar por vez,
+    podemos colocar várias linhas que serão exibidas conforme a quantidade que
+    escolhermos
+    """
     model = StockItems
     extra = 0
 
 @admin.register(Stock)
 class StockAdmin(admin.ModelAdmin):
+    """
+    Chamamos a classe StockItemsInline dentro de inlines e passamos os outros
+    atributos para melhorar a interação com o django admin
+    """
     inlines = (StockItemsInline,)
     list_display = ('ResponsibleUser', 'MovementStatus', 'CreationDate',)
     search_fields = ('ResponsibleUser',)
