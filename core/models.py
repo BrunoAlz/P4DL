a8b9e06087105727b1b3b0234cdaa34016bb937b
Bruno Alves <Bruno6821@gmail.com>
2021-11-03 21:08:46 -0300

    Criando a Model Abstrada do CORE.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    """
+    Model para controle de Criação e alteração de dados no Banco.
+    """
+    CreationDate  = models.DateTimeField(
+        'Criado em:',
+        auto_now_add=True,
+        auto_now=False
+    )
+    ModificationDate  = models.DateTimeField(
+        'Modificado em:',
+        auto_now_add=False,
+        auto_now=True
+    )
+
+    class Meta:
+        """
+        o Abstract indica que essa model será herdada em outras Models,
+        que receberão os Atributos CreationDate e ModificationDate
+        Indicando a data de Criação dos dados no Banco quando eles foram
+        criados e a data de modificação assim que sofrerem alterações
+        """
+        abstract = True
+
